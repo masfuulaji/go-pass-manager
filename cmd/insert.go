4210a8fe325a6ad31fb8ab3e2270a02ee7d6c87f
masfuulaji <masfuulaji@gmail.com>
2023-12-13 18:18:14 +0700

    add saving folder

--- cmd/insert.go
+++ cmd/insert.go
@@ -6,6 +6,7 @@ package cmd
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 
 	"github.com/spf13/cobra"
 	"golang.org/x/term"
@@ -15,6 +16,7 @@ import (
 var insertCmd = &cobra.Command{
 	Use:   "insert",
 	Short: "A brief description of your command",
+	Args:  cobra.ExactArgs(1),
 	Run:   insertPass,
 }
 
@@ -22,14 +24,49 @@ func init() {
 	rootCmd.AddCommand(insertCmd)
 }
 
-var password string
-
-func insertPass(_ *cobra.Command, _ []string) {
+func insertPass(_ *cobra.Command, args []string) {
+	destination := args[0]
 	fmt.Print("Enter password: ")
-	password, err := term.ReadPassword(int(os.Stdin.Fd()))
+
+	password, err := readInput()
 	if err != nil {
-		fmt.Println("Error reading password:", err)
+		fmt.Println("Error reading password :", err)
 		return
 	}
+
+	err = savePassword(destination, password)
+	if err != nil {
+		fmt.Println("Error saving password :", err)
+		return
+	}
+
 	fmt.Println("\nPassword inserted:", string(password))
+}
+
+func readInput() (string, error) {
+	password, err := term.ReadPassword(int(os.Stdin.Fd()))
+	if err != nil {
+		return "", err
+	}
+
+	return string(password), nil
+}
+
+func savePassword(filename, password string) error {
+	stringFile := fmt.Sprintf("password/%s.txt", filename)
+	err := os.MkdirAll(filepath.Dir(stringFile), 0755)
+	if err != nil {
+		return err
+	}
+	file, err := os.Create(stringFile)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	_, err = file.WriteString(password)
+	if err != nil {
+		return err
+	}
+	return nil
 }
